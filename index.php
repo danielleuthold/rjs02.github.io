7595f67fd730d98862c5754166eb7c4de68ad32f
Robin <49201699+xR0B1N@users.noreply.github.com>
2020-02-22 00:33:22 +0100

    Update index.html
    Ergänzung Typora

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <body>
 <h1>Hello World</h1>
 <p>I'm hosted with GitHub Pages. Btw: visit <a href="https://pühh.ch/">pühh.ch</a></p>
+  <h><a href="https://xr0b1n.github.io/>Typora-Stuff</a></h>
 </body>
 </html>
 
